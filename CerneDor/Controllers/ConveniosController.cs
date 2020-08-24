33019ec9c1c8422a72048d5282f203f51f98a514
Pietro Alves <pietrorodrigues@gmail.com>
2020-08-24 10:42:41 -0300

    CRUD Scaffolding Convenio

---
+++ CerneDor/Controllers/ConveniosController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CerneDor.Data;
+using CerneDor.Models;
+
+namespace CerneDor.Controllers
+{
+    public class ConveniosController : Controller
+    {
+        private readonly CerneDorContext _context;
+
+        public ConveniosController(CerneDorContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Convenios
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Convenio.ToListAsync());
+        }
+
+        // GET: Convenios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var convenio = await _context.Convenio
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (convenio == null)
+            {
+                return NotFound();
+            }
+
+            return View(convenio);
+        }
+
+        // GET: Convenios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Convenios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Descricao,CodigoAns")] Convenio convenio)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(convenio);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(convenio);
+        }
+
+        // GET: Convenios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var convenio = await _context.Convenio.FindAsync(id);
+            if (convenio == null)
+            {
+                return NotFound();
+            }
+            return View(convenio);
+        }
+
+        // POST: Convenios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Descricao,CodigoAns")] Convenio convenio)
+        {
+            if (id != convenio.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(convenio);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ConvenioExists(convenio.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(convenio);
+        }
+
+        // GET: Convenios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var convenio = await _context.Convenio
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (convenio == null)
+            {
+                return NotFound();
+            }
+
+            return View(convenio);
+        }
+
+        // POST: Convenios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var convenio = await _context.Convenio.FindAsync(id);
+            _context.Convenio.Remove(convenio);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ConvenioExists(int id)
+        {
+            return _context.Convenio.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ CerneDor/Data/CerneDorContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CerneDor.Models;
+
+namespace CerneDor.Data
+{
+    public class CerneDorContext : DbContext
+    {
+        public CerneDorContext (DbContextOptions<CerneDorContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CerneDor.Models.Convenio> Convenio { get; set; }
+    }
+}
